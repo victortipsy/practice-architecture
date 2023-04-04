1ef490dc8799dd12a14148f224b0265a743e3e3f
victor-okereke <okerekev324@gmail.com>
2023-04-04 09:26:45 -0700

    third wiping

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RouterProvider } from "react-router-dom";
-import { element } from "./routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={element} />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
